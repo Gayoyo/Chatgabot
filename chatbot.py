affca92df8ce06aeb04ff03589ac985d2384eff7
Gayoyo <rizkyjuni.aa@gmail.com>
2025-04-04 22:51:04 +0700

    Fix: update main file and add Procfile

--- chatbot.py
+++ chatbot.py
@@ -67,4 +67,4 @@ def learn():
     return jsonify({"response": "Gagal menyimpan jawaban, pastikan input benar."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
